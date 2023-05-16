d690b0dd3790f516d199acd60bd8284419404909
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-05-16 01:48:04 +0100

    Modified rate model and migration.

--- app/Models/Rate.php
+++ app/Models/Rate.php
@@ -8,4 +8,43 @@ use Illuminate\Database\Eloquent\Model;
 class Rate extends Model
 {
     use HasFactory;
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'product_id',
+        'currency_id',
+        'tenor_id',
+        'minimum_amount',
+        'maximum_amount',
+        'customer_max_rate',
+        'officer_max_rate',
+        'status',
+        'has_direct_debit',
+        'has_no_direct_debit'
+    ];
+
+    /**
+     * Rate belongs to a currency
+     */
+    public function currency()
+    {
+        return $this->belongsTo(Currency::class);
+    }
+
+    /**
+     * Rate belongs to a tenor
+     */
+    public function tenor()
+    {
+        return $this->belongsTo(Tenor::class);
+    }
+
+    /**
+     * Rate belongs to a product
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- database/migrations/2023_05_16_004029_create_rates_table.php
+++ database/migrations/2023_05_16_004029_create_rates_table.php
@@ -15,6 +15,23 @@ return new class extends Migration
     {
         Schema::create('rates', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('tenor_id');
+            $table->unsignedInteger('currency_id');
+            $table->double('minimum_amount', 18, 4);
+            $table->double('maximum_amount', 18, 4);
+            $table->decimal('upfront', 5, 2)->nullable();
+            $table->decimal('maturity', 5, 2)->nullable();
+            $table->decimal('monthly', 5, 2)->nullable();
+            $table->decimal('bi-monthly', 5, 2)->nullable();
+            $table->decimal('quarterly', 5, 2)->nullable();
+            $table->decimal('semi-annually', 5, 2)->nullable();
+            $table->decimal('yearly', 5, 2)->nullable();
+            $table->decimal('officer_max_rate', 5, 2);
+            $table->decimal('customer_max_rate', 5, 2)->nullable();
+            $table->boolean('status')->default(1);
+            $table->decimal('has_direct_debit')->nullable();
+            $table->decimal('has_no_direct_debit')->nullable();
             $table->timestamps();
         });
     }
